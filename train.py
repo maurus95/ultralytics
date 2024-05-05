8abb506402a99ef7247e3d25a30a8906b53b02a2
Maurus Fritsche <maurus.fritsche@assaabloy.com>
2024-05-05 23:32:52 +0200

    etram training

---
+++ train.py
@@ -0,0 +1,36 @@
+from ultralytics import YOLO
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--model", default="yolov8l.yaml")
+    parser.add_argument("-d", "--data", default="yolov8_eb_data.yaml")
+    parser.add_argument("--device", default="0")
+    parser.add_argument("--max_epoch", default=20, type=int)
+    parser.add_argument("--batch_size", default=16, type=int)
+    parser.add_argument("--test", action="store_true")
+    parser.add_argument("--resume", action="store_true")
+
+    args = parser.parse_args()
+    model = YOLO(args.model)
+
+    if args.test:
+        model.val(data=args.data, split="test", amp=False)
+    elif args.resume:
+        model.train(
+            data=args.data,
+            epochs=args.max_epoch,
+            batch=args.batch_size,
+            device=[int(d) for d in args.device.split(",")],
+            amp=False,
+            resume=True,
+        )
+    else:
+        model.train(
+            data=args.data,
+            epochs=args.max_epoch,
+            batch=args.batch_size,
+            device=[int(d) for d in args.device.split(",")],
+            amp=False,
+        )
+        model.val(data=args.data, split="val")
