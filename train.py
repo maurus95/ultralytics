b754d64b23b33f2430b73c1ba2bc333d76e81ecf
Maurus Fritsche <maurus.fritsche@assaabloy.com>
2024-05-07 16:39:48 +0200

    update training arguments and script

--- train.py
+++ train.py
@@ -6,11 +6,13 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--model", default="yolov8l.yaml")
     parser.add_argument("-d", "--data", default="yolov8_eb_data.yaml")
     parser.add_argument("--device", default="0")
+    parser.add_argument("--workers", default=8, type=int)
     parser.add_argument("--max_epoch", default=20, type=int)
     parser.add_argument("--batch_size", default=16, type=int)
     parser.add_argument("--test", action="store_true")
     parser.add_argument("--resume", action="store_true")
     parser.add_argument("--amp", action="store_true")
+    parser.add_argument("--cache", action="store_true")
 
     args = parser.parse_args()
     model = YOLO(args.model)
@@ -25,6 +27,8 @@ if __name__ == "__main__":
             epochs=args.max_epoch,
             batch=args.batch_size,
             device=[int(d) for d in args.device.split(",")],
+            workers=args.workers,
+            resume=args.resume,
             amp=args.amp,
+            cache=args.cache,
         )
-        model.val(data=args.data, split="val")
--- train.sh
+++ train.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # start training
+# MP_NUM_THREADS=1
 NCCL_P2P_LEVEL=NVL PYTHONPATH=/home/maufri/repos/ultralytics /usr/bin/env /home/maufri/miniconda3/envs/pt-gpu/bin/python \
 train.py -d /home/maufri/repos/ultralytics/yolov8_eb_data.yaml --batch_size 32 --device 3 --amp
+# train.py -m /home/maufri/repos/ultralytics/runs/detect/train4/weights/last.pt --resume
